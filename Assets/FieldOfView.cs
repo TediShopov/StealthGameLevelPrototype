49e2a85f7ae9b8b60d084bf4cbc0ec42b8e76513
TediShopov <tedikatana@abv.bg>
2023-10-26 12:22:08 +0100

    Enemy can detect player and level is ended

--- Assets/FieldOfView.cs
+++ Assets/FieldOfView.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class FieldOfView : MonoBehaviour
 {
     public MeshFilter meshFilter;
+    public GameObject SearchedObject;
     Mesh mesh;
     public float FOV = 90.0f;
     public float ViewDistance = 50f;
@@ -18,9 +20,7 @@ public class FieldOfView : MonoBehaviour
         mesh=new Mesh();
         meshFilter.mesh = mesh;
     }
-
-    // Update is called once per frame
-    void Update()
+    public void RebuidMeshComponent() 
     {
         Vector3 LocalOrigin = Vector3.zero;
         Vector3 GlobalOrigin = this.transform.position;
@@ -73,11 +73,41 @@ public class FieldOfView : MonoBehaviour
         mesh.uv = uv;
         mesh.triangles = triangles;
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RebuidMeshComponent();
+        if (TestCollisition(SearchedObject.transform.position))
+        {
+            Debug.Log("Game is lost");
+            SceneManager.LoadScene(0);
+        }
+    }
+    public Vector3 GetGlobalDirection() 
+    {
+        float modifer = Helpers.GetAngleFromVectorFloat(this.transform.right);
+        return Helpers.GetVectorFromAngle(PointingAngle + modifer);
+    }
+    public bool TestCollisition(Vector3 testPosition) 
+    {
+        //Gloabar direction vector 
+        Vector3 globalPointingDirection = GetGlobalDirection().normalized;
+        Vector3 vectorToTarget = (testPosition- transform.position).normalized;
+        float angle = Vector3.Angle(globalPointingDirection, vectorToTarget);
+        if (angle < FOV / 2.0f && Vector3.Distance(testPosition, this.transform.position) <= ViewDistance) 
+        {
+            Debug.Log($"Object is detected");
+            return true;
+        }
+        return false;
+    }
     public float GetStartinAngle ()
     {
         return (PointingAngle + FOV / 2.0f); 
     }
     public void OnDrawGizmos()
     {
+        Debug.DrawRay(this.transform.position, GetGlobalDirection(), Color.yellow);
     }
 }
