039bb85f867fc127452292b4472afa82e7551262
TediShopov <tedikatana@abv.bg>
2024-01-28 16:12:10 +0000

    Refactored random path dfs algorithm

--- Assets/PathGeneratorClass.cs
+++ Assets/PathGeneratorClass.cs
@@ -18,22 +18,6 @@ public class PathGeneratorClass : MonoBehaviour, IPathGenerator
         Random.InitState(seed: RandomPathSeed);
     }
     public Graph<Vector2> Roadmap { get; set; }
-//    virtual public List<Vector2> GeneratePath() 
-//    {
-//        var keyValueList = this.Roadmap.adjacencyList.Keys.ToArray();
-//
-//        // Generate a random index to pick an element.
-//        int randomIndex = UnityEngine.Random.Range(0,this.Roadmap.adjacencyList.Count);
-//
-//        
-//        var path = new List<Vector2>();
-//        for (int i = 0; i < AttemptsToMatchBiasedDistance; i++)
-//        {
-//            RandomPathDFS(keyValueList[randomIndex], new HashSet<Vector2>(), path, BiasPathDistance);
-//            if (PathLength(path) >= BiasPathDistance) { break; }
-//        }
-//        return path;
-//    }
     virtual public List<List<Vector2>> GeneratePaths(int pathsToGenerate) 
     {
         var keyValueList = this.Roadmap.adjacencyList.Keys.ToArray();
@@ -49,82 +33,37 @@ public class PathGeneratorClass : MonoBehaviour, IPathGenerator
             {
                 // Generate a random index to pick an element.
                 int randomIndex = UnityEngine.Random.Range(0,this.Roadmap.adjacencyList.Count);
-                RandomPathDFS(keyValueList[randomIndex], visited, path, ref bestFoundPath,BiasPathDistance);
+                RandomPathDFS(keyValueList[randomIndex], ref visited, ref path, ref bestFoundPath,BiasPathDistance);
                 if (PathLength(bestFoundPath) >= BiasPathDistance) { break; }
             }
             pathsToReturn.Add(bestFoundPath);
         }
         return pathsToReturn;
     }
-    private float RandomPathDFS(Vector2 currentNode, HashSet<Vector2> visited, List<Vector2> path, ref List<Vector2> bestPath,float maxDistance)
+    private void RandomPathDFS(Vector2 currentNode,ref HashSet<Vector2> visited, ref List<Vector2> currentPath, ref List<Vector2> bestPath,float maxDistance)
     {
         visited.Add(currentNode);
-        path.Add(currentNode);
-        
+        currentPath.Add(currentNode);
 
         List<Vector2> unvisitedNeighbors = Roadmap.GetUnvisitedNeighbors(currentNode, visited);
 
-        if (PathLength(path) > PathLength(bestPath))
+        float coveredDistance = PathLength(currentPath);
+        if (coveredDistance < maxDistance && unvisitedNeighbors.Count != 0)
         {
-            bestPath = new List<Vector2>(path);
+            //Update best path
+            if (coveredDistance > PathLength(bestPath))
+                bestPath = new List<Vector2>(currentPath);
+            // Shuffle the neighbors randomly.
+            Roadmap.Shuffle(unvisitedNeighbors);
+            foreach (var neighbor in unvisitedNeighbors)
+            {
+                RandomPathDFS(neighbor,ref visited, ref currentPath,ref bestPath,maxDistance);
+            }
         }
-        float coveredDistance = PathLength(path);
-        if (unvisitedNeighbors.Count == 0 || coveredDistance > maxDistance)
-            return coveredDistance;
-
-        // Shuffle the neighbors randomly.
-        Roadmap.Shuffle(unvisitedNeighbors);
-
-        foreach (var neighbor in unvisitedNeighbors)
-        {
-            float newPathDistance = RandomPathDFS(neighbor, visited, path,ref bestPath,maxDistance);
-            //Rollback changes in dfs
-            //If path doesnt satisfy criteria remove the nodes from it and try the next neighbour;
-            int index = path.FindIndex(x => x == currentNode);
-            path.RemoveRange(index + 1, path.Count - index - 1);
-            //Clear from visited
-            visited.Remove(neighbor);
-        }
-        return PathLength(path);
+        //Rollback changes 
+        currentPath.RemoveAt(currentPath.Count - 1);
+        visited.Remove(currentNode);
     }
-//    private float RandomPathDFS(   Vector2 currentNode, HashSet<Vector2> visited, List<Vector2> path, List<Vector2> bestPath,float maxDistance)
-//    {
-//        visited.Add(currentNode);
-//        path.Add(currentNode);
-//        
-//
-//        List<Vector2> unvisitedNeighbors = Roadmap.GetUnvisitedNeighbors(currentNode, visited);
-//
-//        if (PathLength(path) > PathLength(bestPath))
-//        {
-//            bestPath = new List<Vector2>(path);
-//        }
-//        float coveredDistance = PathLength(path);
-//        if (unvisitedNeighbors.Count == 0 || coveredDistance > maxDistance)
-//            return coveredDistance;
-//
-//        // Shuffle the neighbors randomly.
-//        Roadmap.Shuffle(unvisitedNeighbors);
-//
-//        foreach (var neighbor in unvisitedNeighbors)
-//        {
-//            float distToNeighbor= Vector2.Distance(currentNode, neighbor);
-//            if (RandomPathDFS(neighbor, visited, path, bestPath,maxDistance)  >maxDistance) 
-//            {
-//                return PathLength(path);
-//            }
-//            else
-//            {
-//
-//                //If path doesnt satisfy criteria remove the nodes from it and try the next neighbour;
-//                int index = path.FindIndex(x=>x==currentNode);
-//                path.RemoveRange(index+1, path.Count - index - 1);
-//                //Clear from visited
-//                visited.Remove(neighbor);
-//            }
-//        }
-//        return PathLength(path);
-//    }
     public float PathLength(List<Vector2> path) 
     {
         float length = 0;
