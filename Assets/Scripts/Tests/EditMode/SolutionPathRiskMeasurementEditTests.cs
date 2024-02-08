a49c0595a0dc010f30a2c800a966df2b4a2449ba
TediShopov <tedikatana@abv.bg>
2024-02-08 17:22:01 +0000

    Bug fixing and more tests for RiskMeasurement

--- Assets/Scripts/Tests/EditMode/SolutionPathRiskMeasurementEditTests.cs
+++ Assets/Scripts/Tests/EditMode/SolutionPathRiskMeasurementEditTests.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
 using System.EnterpriseServices;
+using System.IdentityModel.Tokens;
 using System.Linq;
 using System.Threading;
 using NUnit.Framework;
@@ -125,21 +126,26 @@ LayerMask mask
 
     public float RiskFromAngle(FutureTransform player, FutureTransform enemy)
     {
-        Vector2 directionToPlayer = (enemy.Position - player.Position).normalized;
-        float sangle = Vector2.SignedAngle(enemy.Direction, directionToPlayer);
+        Vector2 directionToPlayer = (player.Position - enemy.Position).normalized;
+        float angle = Vector2.Angle(enemy.Direction, directionToPlayer);
         //Is in view range
-        if (sangle < DefaultEnemyProperties.FOV)
+        if (angle < DefaultEnemyProperties.FOV / 2.0f)
         {
             return 1;
         }
-        float relAngularCost = Mathf.InverseLerp(180, DefaultEnemyProperties.FOV, sangle);
-        return Mathf.Lerp(0, 1, relAngularCost);
+        float relAngularCost = Mathf.InverseLerp(180, DefaultEnemyProperties.FOV / 2.0f, angle);
+        return Mathf.Lerp(0.5f, 1, relAngularCost);
     }
 }
 
 public class SolutionPathRiskMeasurementEditTests
 {
-    [SerializeField] public CharacterController2D CharacterController2D;
+    public DefaultEnemyProperties EnemyProperties = new DefaultEnemyProperties()
+    {
+        FOV = 45,
+        ViewDistance = 1.0f,
+        Speed = 1.0f
+    };
 
     [Test]
     public void NoPatrolPaths_RiskIsZero()
@@ -155,33 +161,34 @@ public class SolutionPathRiskMeasurementEditTests
             new FieldOfViewSolutionRiskMeasurement(
                 new SolutionPath(solutionPathRaw),
                 new List<PatrolPath>(),
-                new DefaultEnemyProperties(),
+               EnemyProperties,
                 LayerMask.GetMask("Obstacles"));
         Assert.AreEqual(0, solutionPathRiskMeasurement.OverallRisk(1), 0.001f);
     }
 
-    [Test]
-    public IEnumerator FovLineOfSightBlocked_RiskIsZero()
-    {
-        //Stationary guard
-        List<Vector3> solutionPathRaw = new List<Vector3>()
-        {
-            new Vector3(-3,0,0),
-            new Vector3(-2,0,1),
-            new Vector3(-1,0,2)
-        };
-        var enemyPath = new GameObject("", new Type[] { typeof(PatrolPath) }).GetComponent<PatrolPath>();
-        var obstacle = new GameObject("", new Type[] { typeof(Rigidbody2D), typeof(BoxCollider2D) })
-            .GetComponent<BoxCollider2D>();
-        yield return null;
-        ISolutionPathRiskMeasurement solutionPathRiskMeasurement =
-            new FieldOfViewSolutionRiskMeasurement(
-                new SolutionPath(solutionPathRaw),
-                new List<PatrolPath>() { enemyPath },
-                new DefaultEnemyProperties(),
-                LayerMask.GetMask("Obstacles"));
-        Assert.AreEqual(0, solutionPathRiskMeasurement.OverallRisk(1), 0.001f);
-    }
+    //TODO move to play tests
+    //    [Test]
+    //    public IEnumerator FovLineOfSightBlocked_RiskIsZero()
+    //    {
+    //        //Stationary guard
+    //        List<Vector3> solutionPathRaw = new List<Vector3>()
+    //        {
+    //            new Vector3(-3,0,0),
+    //            new Vector3(-2,0,1),
+    //            new Vector3(-1,0,2)
+    //        };
+    //        var enemyPath = new GameObject("", new Type[] { typeof(PatrolPath) }).GetComponent<PatrolPath>();
+    //        var obstacle = new GameObject("", new Type[] { typeof(Rigidbody2D), typeof(BoxCollider2D) })
+    //            .GetComponent<BoxCollider2D>();
+    //        yield return null;
+    //        ISolutionPathRiskMeasurement solutionPathRiskMeasurement =
+    //            new FieldOfViewSolutionRiskMeasurement(
+    //                new SolutionPath(solutionPathRaw),
+    //                new List<PatrolPath>() { enemyPath },
+    //               EnemyProperties,
+    //                LayerMask.GetMask("Obstacles"));
+    //        Assert.AreEqual(0, solutionPathRiskMeasurement.OverallRisk(1), 0.001f);
+    //    }
 
     [UnityTest]
     public IEnumerator FovLineOfSightNonBlocked_RiskIsNonZero()
@@ -202,27 +209,47 @@ public class SolutionPathRiskMeasurementEditTests
             new FieldOfViewSolutionRiskMeasurement(
                 new SolutionPath(solutionPathRaw),
                 new List<PatrolPath>() { enemyPath },
-                new DefaultEnemyProperties(),
+               EnemyProperties,
                 LayerMask.GetMask("Obstacles"));
         Assert.AreNotEqual(0, solutionPathRiskMeasurement.OverallRisk(1));
         yield return null;
     }
 
     [Test]
-    public void StationaryGuard_CorrectMeasurement()
+    public void AngleRiskCost_IsCorrectnes()
     {
-        //        //Stationary guard
-        //        List<Vector3> solutionPath = new List<Vector3>()
-        //        {
-        //            new Vector3(0,0,0),
-        //            new Vector3(1,0,1),
-        //            new Vector3(2,0,2)
-        //        };
-        //        var standingPatrolPaths = new PatrolPath();
-        //        standingPatrolPaths.transform.position = new Vector3(3, 0, 0);
-        //        float actual = MeasureRisk(
-        //            solutionPath, new List<PatrolPath>());
-        //        Assert.AreEqual(2, actual, 0.001f);
+        FutureTransform player = new FutureTransform();
+        player.Position = new Vector2(0, 0);
+        player.Direction = new Vector2(0, 0);
+
+        FutureTransform enemy = new FutureTransform();
+        enemy.Position = new Vector2(0, 0);
+        enemy.Direction = Vector2.right;
+
+        ///
+        FieldOfViewSolutionRiskMeasurement solutionPathRiskMeasurement =
+            new FieldOfViewSolutionRiskMeasurement(
+                new SolutionPath(new List<Vector3>()),
+                new List<PatrolPath>(),
+               EnemyProperties,
+                LayerMask.GetMask("Obstacles"));
+
+        player.Position = new Vector2(0, 0);
+        //Directly on top should return 1
+        Assert.AreEqual(1.0f, solutionPathRiskMeasurement.RiskFromAngle(player, enemy));
+        //Anywhere in visibility should return 1
+        player.Position = Quaternion.AngleAxis(15, Vector3.forward) * Vector2.right;
+        Assert.AreEqual(1.0f, solutionPathRiskMeasurement.RiskFromAngle(player, enemy));
+        player.Position = Quaternion.AngleAxis(-15, Vector3.forward) * Vector2.right;
+        Assert.AreEqual(1.0f, solutionPathRiskMeasurement.RiskFromAngle(player, enemy));
+        //Directly opposite return minimum of function 0,5
+        player.Position = Quaternion.AngleAxis(180, Vector3.forward) * Vector2.right;
+        Assert.AreEqual(0.5f, solutionPathRiskMeasurement.RiskFromAngle(player, enemy));
+        //At center of (180 - angleFov)/2.0f return 0.75
+        //float n = (180 - EnemyProperties.FOV / 2.0f) / 2.0f;
+        float n = (EnemyProperties.FOV / 2.0f) + (180 - EnemyProperties.FOV / 2.0f) * 0.5f;
+        player.Position = Quaternion.AngleAxis(n, Vector3.forward) * Vector2.right;
+        Assert.AreEqual(0.75f, solutionPathRiskMeasurement.RiskFromAngle(player, enemy));
     }
 
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
